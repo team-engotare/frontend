80efee22378600d8121fd83bb4abade7f9f3ecd6
Shirleen-Gathungu <gathungushirleen@gmail.com>
2023-08-24 16:16:57 +0300

    added uploading button in dashboard, added notifications and profile in header

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,24 +1,97 @@
-import React from "react";
-import { styled } from '@mui/system';
-import { Typography } from '@mui/material';
+  import React from "react";
+  import { styled } from '@mui/system';
+  import { Typography,Button } from '@mui/material';
+  import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
 
+  const StyledH1 = styled(Typography)({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    marginTop:'89px',
+    marginLeft:'293px',
+    fontSize:'30px',
+    fontFamily:'cursive'
+  });
+  const StyledH2 = styled(Typography)({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    marginTop:'29px',
+    marginLeft:'293px',
+    fontSize:'24px',
+    fontFamily:'cursive'
+  });
 
-const StyledH1 = styled(Typography)({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-start',
-  marginTop:'89px',
-  marginLeft:'293px',
-  fontSize:'30px',
-  fontFamily:'cursive'
-});
-const Dashboard = () => {
-  return (
-    <div>
-      <StyledH1 variant="h1">Data</StyledH1>
-    </div>
+    const StyledButton = styled(Button)({
+      display:'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginLeft:'443px',
+      backgroundColor: '#8E5315',
+      width: '382px',
+      paddingTop:'8px',
+      paddingBottom:'6px',
+      paddingRight:'1px',
+      paddingLeft:'1px',
+      marginTop:'158px',
+      textAlign: 'center',
+      color: 'white',
+      whiteSpace: 'nowrap', 
+      '&:hover': {
+          backgroundColor: 'darkbrown'
+      }
+  });
+  const StyledArrowCircleUpIcon = styled(ArrowCircleUpIcon)({
+    color: 'white',
+    boxShadow: '0 0 0 2px darkbrown', // Adjust the "2px" value to make the outline thicker or thinner
+    borderRadius: '50%', // Makes the boxShadow circular
+  }); 
+
+  const Dashboard = () => {
+
+    const fileInputRef = React.useRef(null);
+
+    const handleButtonClick = () => {
+        // Trigger the file input click event
+        fileInputRef.current.click();
+    };
+
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            console.log("Selected file:", file.name);
+            // TODO: Handle the file upload logic here
+        }
+    };
+
+ 
     
-  );
-};
+    return (
+      <div>
+        <StyledH1 variant="h1">Data</StyledH1>
+        <StyledH2 variant="h2">Lions Identified</StyledH2>
 
-export default Dashboard;
+        <input
+                  type="file"
+                  style={{ display: 'none' }}
+                  ref={fileInputRef}
+                  onChange={handleFileChange}
+              />
+
+              {/* Material-UI button that triggers the hidden file input */}
+              <StyledButton 
+                variant="contained" 
+                color="primary" 
+                startIcon={<StyledArrowCircleUpIcon style={{ fontSize: 60 }} />} 
+                onClick={handleButtonClick}
+                
+                >
+                  Upload Images for Identification
+              </StyledButton>
+
+      </div>
+      
+    );
+  };
+
+  export default Dashboard;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,138 @@
-import React from "react";
+import React, { useState } from "react";
 import { styled } from '@mui/system';
-import { AppBar, Toolbar } from '@mui/material';
-import {Notifications} from '@mui/icons-material';
+import { AppBar, Toolbar, Badge, Popover, List, ListItem, ListItemText, IconButton, Avatar, Menu, MenuItem } from '@mui/material';
+import { Notifications } from '@mui/icons-material';
 
 const StyledAppBar = styled(AppBar)({
-    // Add your styles here
     backgroundColor: '#DF8530',
-  });
+});
 
-  const StyledNotifications = styled(Notifications)({
-    color:'#F5DAC1',
-    marginRight:'9px',
-    height:'21px',
-    marginBottom:'3px'
-  
-  });
+const StyledNotifications = styled(Notifications)({
+    color: '#F5DAC1',
+    marginRight: '9px',
+    height: '21px',
+    marginBottom: '-2px',
+    marginTop: '8px'
+});
 
-  const StyledToolbar = styled(Toolbar)({
-    // Add your styles here
-    justifyContent: 'space-between',
-  });
+const StyledToolbar = styled(Toolbar)({
+    display: 'flex',
+    justifyContent: 'flex-end',
+});
 
-  
- 
-const Header = () =>{
-    return(
+const StyledBadge = styled(Badge)({
+    marginRight: '9px',
+});
+
+const ActionsContainer = styled('div')({
+    display: 'flex',
+    alignItems: 'center',
+});
+
+const StyledIconButton = styled(IconButton)({
+    marginLeft: '16px',
+});
+
+const Header = () => {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [profileAnchorEl, setProfileAnchorEl] = useState(null);
+
+    const notificationsCount = 5;
+
+    const notifications = [
+        "Notification 1",
+        "Notification 2",
+        "Notification 3",
+        "Notification 4",
+        "Notification 5"
+    ];
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleProfileClick = (event) => {
+        setProfileAnchorEl(event.currentTarget);
+    };
+
+    const handleProfileClose = () => {
+        setProfileAnchorEl(null);
+    };
+
+    const handleLogout = () => {
+        console.log('User logged out!');
+        handleProfileClose();
+    };
+
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
+    const profileOpen = Boolean(profileAnchorEl);
+    const profileId = profileOpen ? 'profile-popover' : undefined;
+
+    return (
         <StyledAppBar position="fixed">
+            <StyledToolbar>
+                <ActionsContainer>
+                    <StyledBadge badgeContent={notificationsCount} color="secondary">
+                        <StyledNotifications onClick={handleClick} />
+                    </StyledBadge>
 
-        <StyledToolbar>
+                    <StyledIconButton onClick={handleProfileClick} >
+                    <Avatar 
+                        alt="User Name" 
+                        src="/path-to-your-image.jpg"
+                        style={{ width: '32px', height: '32px', marginTop:'4px'}}  // Adjust width and height as per your needs
+                        />                    
+                      </StyledIconButton>
 
-        <StyledNotifications/>
+                    <Menu
+                        id={profileId}
+                        anchorEl={profileAnchorEl}
+                        open={profileOpen}
+                        onClose={handleProfileClose}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'right',
+                        }}
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                    >
+                        <MenuItem onClick={handleProfileClose}>Profile</MenuItem>
+                        <MenuItem onClick={handleProfileClose}>Settings</MenuItem>
+                        <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                    </Menu>
+                </ActionsContainer>
 
-        </StyledToolbar>
-
-      </StyledAppBar>
+                <Popover
+                    id={id}
+                    open={open}
+                    anchorEl={anchorEl}
+                    onClose={handleClose}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'center',
+                    }}
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'center',
+                    }}
+                >
+                    <List>
+                        {notifications.map((notification, index) => (
+                            <ListItem key={index}>
+                                <ListItemText primary={notification} />
+                            </ListItem>
+                        ))}
+                    </List>
+                </Popover>
+            </StyledToolbar>
+        </StyledAppBar>
     )
 }
 
